33c2212af02af76f1ad948f5cdad62019ea37a5a
Dolan <you@example.com>
2026-03-07 03:57:40 +0100

    fix env glitch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: "segment-worker",
       script: "dist/server.js",
+      node_args: "--env-file=.env",
       instances: 1,
       autorestart: true,
       watch: false,
